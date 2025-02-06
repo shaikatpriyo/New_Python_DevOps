6db9a600bfef1f71d28d1aeed54a1e1c4755c151
Shaikat <sshaikat.ku@gmail.com>
2025-02-07 03:06:49 +0600

    Add Fizzbuzz problem

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+n = int(input("Enter the number:"))
+
+if n % 15 == 0 :
+    print ("FizzBuzz")
+
+elif n % 5 == 0 :
+    print ("Buzz")
+
+elif n % 3 == 0 :
+    print ("Fizz")
+
+else :
+    print ("not valid")
+
