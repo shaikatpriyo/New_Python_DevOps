6e876ac06fc30b71f0ed2aa4546156ac4ad21624
Shaikat <sshaikat.ku@gmail.com>
2025-02-07 11:19:56 +0600

    Add my list

---
+++ list_data.py
@@ -0,0 +1,10 @@
+#position/index
+
+my_list_1=['Suresh', 'Gonesh', 'Paresh']
+my_list_2=['Sobuj','Tamim','Zulkar']
+
+my_list_1.append('Emon')
+my_list_2.insert(1, 'Atik')
+
+New_Group= my_list_2 + my_list_1
+print(New_Group)
