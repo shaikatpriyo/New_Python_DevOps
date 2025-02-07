989e34f9a18e5221aea86783f4c3039124afe2de
Shaikat <sshaikat.ku@gmail.com>
2025-02-08 00:22:06 +0600

    Add loops

---
+++ loops.py
@@ -0,0 +1,10 @@
+fruits=['orange','mango', 'banana', 'strawberry', 'Guava']
+
+#iteration
+#For loop
+
+for fruit in fruits:
+    if fruit =='Guava':
+        print('Guava Paiche')
+    else :
+        print('painai')
