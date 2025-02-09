b819d764d14804da6cd76708a5b8ffa1f06e1144
Shaikat <sshaikat.ku@gmail.com>
2025-02-10 00:00:21 +0600

    Add fruits

--- membership_operators.py
+++ membership_operators.py
@@ -2,7 +2,8 @@
 print("Shaikat" in "I am shaikat") #False
 print("shaikat" in "I am shaikat") #true
 
-Fruits = ['Banana', 'Grape']
+Fruits = ['Banana', 'Grape', 'berry']
 print('Banana' in Fruits)
 print('Mango' in Fruits)
+print('berry' in Fruits)
 
